2327059b431ed23519d42905f37994b9f35c38e4
Abhishek Kumar <127936615+Akumar4318@users.noreply.github.com>
2023-06-27 20:39:07 +0530

    Create ans4.md

---
+++ ans4.m
@@ -0,0 +1,13 @@
+DataFrame=it is a spreadsheet like structure representing rows which contains one or mulyiple columns.
+series - it is a one dimensional array with indexs,it stores single row and column of a data in a dataframe
+example dataframe= name  class roll no.
+                 aman     5     01
+                pal       5      02
+              abhay       6      07
+
+
+
+ series = name
+          aman
+          pal
+         abhay
